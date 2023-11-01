c70ae3bad264e8adac2187ddb3800f9c90c6da76
source-net <59995451+source-net@users.noreply.github.com>
2023-11-01 19:12:32 +0900

    cicd & yml

--- src/main/java/com/example/jenkins/controller/UtilController.java
+++ src/main/java/com/example/jenkins/controller/UtilController.java
@@ -3,17 +3,19 @@ package com.example.jenkins.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
 @RestController
 @RequiredArgsConstructor
-public class CicdSupportApi {
+@RequestMapping("/utils")
+public class UtilController {
     private final Environment env;
 
-    @GetMapping("/status/profile")
-    public String getProfileName() {
+    @GetMapping("/profile")
+    public String getProfile() {
         return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 }
