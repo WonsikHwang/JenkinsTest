25d4e60eed5222cf9915acf27d4665750ce33e04
source-net <j2s0007@gmail.com>
2023-11-03 11:18:38 +0900

    doc TEST
    test

--- src/main/java/com/example/jenkins/repository/BoardRepository.java
+++ src/main/java/com/example/jenkins/repository/BoardRepository.java
@@ -8,7 +8,4 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query(value = "select now()", nativeQuery = true)
     public String test();
-
-    @Query(value = "show databases", nativeQuery = true)
-    public String test2();
 }
